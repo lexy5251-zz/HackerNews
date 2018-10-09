b66c819df7f918999675b601d3bdd059c3580fa4
Xi <xixi5251@gmail.com>
2018-10-09 00:05:30 -0400

    even number

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,43 +1,46 @@
-$(function() {
-    $.ajax({
-        type: 'GET',
-        url: `https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`,
-    }).then(function(data) {
+retrieveAndRender(0);
 
-        let number = 0;
-        let dataPartial = data.slice(number, number + 30);
-        loadThirty();
+function retrieveAndRender(mode) {
+    $(function() {
+        $.ajax({
+            type: 'GET',
+            url: `https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`,
+        }).then(function(data) {
 
-        $("#load-button").click(function() {
-            loadThirty()
-        });
+            let number = 0;
+            let dataPartial = data.slice(number, number + 30);
+            loadThirty();
 
-        function loadThirty() {
-            dataPartial.forEach(function(index) {
-                let storyUrl = `https://hacker-news.firebaseio.com/v0/item/${index}.json?print=pretty`;
-                console.log("storyUrl is: ",storyUrl);
-                $.ajax({
-                    type: 'GET',
-                    url: storyUrl,
-                }).then(function(element) {
-                    console.log(storyUrl);
-                    createViewForItem(element);
+            $("#load-button").click(function() {
+                loadThirty()
+            });
 
+            function loadThirty() {
+                dataPartial.forEach(function(index) {
+                    if (mode === 0 || (mode === 1 && index % 2 === 1) || (mode === 2 && index % 2 === 0)) {
+                        let storyUrl = `https://hacker-news.firebaseio.com/v0/item/${index}.json?print=pretty`;
+                        console.log("storyUrl is: ", storyUrl);
+                        $.ajax({
+                            type: 'GET',
+                            url: storyUrl,
+                        }).then(function(element) {
+                            console.log(storyUrl);
+                            createViewForItem(element);
+                        });
+                    }
+                });
 
-                })
+                number = number + 30;
+                if (number >= 500)
+                    $("load-button").attr("disabled", true);
 
+                dataPartial = data.slice(number, number + 30);
+            };
 
-            })
+        })
+    });
+}
 
-            number = number + 30;
-            if (number >= 500)
-                $("load-button").attr("disabled", true);
-
-            dataPartial = data.slice(number, number + 30);
-        };
-
-    })
-});
 
 function createViewForItem(element) {
 
@@ -50,21 +53,38 @@ function createViewForItem(element) {
     }).then(function(data) {
         let imageUrl = data.image;
 
-    console.log("data is ",data);
-    if (imageUrl) {
-        $('.articles').append(`<div class="card"><img src=${imageUrl} />
+        console.log("data is ", data);
+        if (imageUrl) {
+            $('.articles').append(`<div class="card"><img src=${imageUrl} />
                                <div class="card-content">
                                <div class="author">${element.by}</div>
                                <h3 class="card-h3">${element.title}</h3>
                                </div>
                                </div>
                             `);
-    } else {
-        $('.articles').append(`<h3>${element.title}</h3>
+        } else {
+            $('.articles').append(`<div class="card">
+                               <img src="images/trade-war-final.jpg" />
+                               <div class="card-content">
+                               <div class="author">${element.by}</div>
+                               <h3><h3 class="card-h3">${element.title}</h3>
+                               </div>
+                               </div>
                               `);
-    }
+        }
 
-  });
+    });
 
 }
 
+$("#all-stories").click(function() {
+    retrieveAndRender(0);
+});
+
+$("#odd-stories").click(function() {
+    retrieveAndRender(1);
+});
+
+$("#even-stories").click(function() {
+    retrieveAndRender(2);
+});
